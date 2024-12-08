08bd9b4ac64ff4afefde9410293bb4ed006ff221
Bubhux <bubhuxpaindepice@gmail.com>
2024-12-08 17:11:59 +0100

    Mise à jour V.2.8  Changes to be committed: 	modified:   app/routes/home/skills.jsx 	modified:   app/utils/skills-image.js 	new file:   app/utils/skills-links.js 	modified:   app/utils/skills.js 	new file:   static/svg/skills/flask.svg 	new file:   static/svg/skills/postman.svg 	new file:   static/svg/skills/sass.svg 	new file:   static/svg/skills/sentry.svg 	renamed:    static/svg/skills/vue.svg -> static/svg/skills/vuejs.svg

---
+++ app/utils/skills-links.js
@@ -0,0 +1,84 @@
+// apps/utils/skills-links.js
+
+export const skillLinks = {
+    'JavaScript': {
+        url: 'https://developer.mozilla.org/fr/docs/Web/JavaScript',
+        isExternal: true
+    },
+    'Python': {
+        url: 'https://www.python.org/',
+        isExternal: true
+    },
+    'HTML': {
+        url: 'https://developer.mozilla.org/fr/docs/Web/HTML',
+        isExternal: true
+    },
+    'CSS': {
+        url: 'https://developer.mozilla.org/fr/docs/Web/CSS',
+        isExternal: true
+    },
+    'SASS': {
+        url: 'https://sass-lang.com/',
+        isExternal: true
+    },
+    'Tailwind': {
+        url: 'https://tailwindcss.com/',
+        isExternal: true
+    },
+    'Django': {
+        url: 'https://www.djangoproject.com/',
+        isExternal: true
+    },
+    'Bootstrap': {
+        url: 'https://getbootstrap.com/',
+        isExternal: true
+    },
+    'React': {
+        url: 'https://react.dev/',
+        isExternal: true
+    },
+    'Flask': {
+        url: 'https://flask.palletsprojects.com/en/3.0.x/',
+        isExternal: true
+    },
+    'MySQL': {
+        url: 'https://www.mysql.com/',
+        isExternal: true
+    },
+    'SQLite': {
+        url: 'https://www.sqlite.org/',
+        isExternal: true
+    },
+    'MongoDB': {
+        url: 'https://www.mongodb.com/',
+        isExternal: true
+    },
+    'ViteJS': {
+        url: 'https://v2.vitejs.dev/',
+        isExternal: true
+    },
+    'VueJS': {
+        url: 'https://vuejs.org/',
+        isExternal: true
+    },
+    'Docker': {
+        url: 'https://www.docker.com/',
+        isExternal: true
+    },
+    'Selenium': {
+        url: 'https://www.selenium.dev/',
+        isExternal: true
+    },
+    'Sentry': {
+        url: 'https://sentry.io/welcome/',
+        isExternal: true
+    },
+    'Postman': {
+        url: 'https://www.postman.com/',
+        isExternal: true
+    },
+    'Git': {
+        url: 'https://git-scm.com/',
+        isExternal: true
+    },
+};

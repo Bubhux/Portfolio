566f738e12550421555be10bb4b82d0e8b211205
Bubhux <bubhuxpaindepice@gmail.com>
2024-12-17 05:55:40 +0100

    Mise à jour V.3.7  Changes to be committed: 	new file:   app/assets/img-projects/laptop-crm.png 	modified:   app/components/cards/cards.module.css 	modified:   app/components/cards/project-cards.jsx 	modified:   app/components/cards/projects.jsx 	modified:   app/components/divider/divider.jsx 	modified:   app/components/divider/divider.module.css 	modified:   app/routes/home/home.jsx

--- app/components/cards/projects.jsx
+++ app/components/cards/projects.jsx
@@ -1,23 +1,70 @@
 // app/components/cards/projects.jsx
-import React from "react";
+import React, { useState } from "react";
 import { Cards } from './project-cards';
-import laptopTodo from '~/assets/img-projects/laptop-todo.png';
+
 import styles from './cards.module.css';
+import { Transition } from '~/components/transition';
+import { media } from '~/utils/style';
+import { useWindowSize } from '~/hooks';
+import { Divider } from '~/components/divider';
+import { Section } from '~/components/section';
 
 
-export function Projects() {
-    return (
-        <div className={styles.projectSection}>
-            <div className={styles.projectCards}>
-                <Cards
-                    imgPath={laptopTodo}
-                    isBlog={false}
-                    title="Todo List with Calendar Integration"
-                    description="Designing and developing a todo list app with integrated calendar features to help users organize tasks and schedule events efficiently."
-                    ghLink="https://github.com/Bubhux/App-TodoListCalendar"
-                    demoLink="https://bubhux.github.io/Projet-Web-HTML-CSS/"
-                />
+export function Projects({ id, visible: sectionVisible, title, description, imgPath, ghLink, demoLink, isBlog, alternate, ...rest }) {
+
+    const [focused, setFocused] = useState(false);
+    const { width } = useWindowSize();
+    const isMobile = width <= media.tablet;
+
+    function renderDetails(visible) {
+        return (
+            <div className={styles.details} data-visible={visible}>
+                <div aria-hidden className={styles.index}>
+                    <Divider
+                        notchWidth="64px"
+                        notchHeight="0px"
+                        collapsed={!visible}
+                        collapseDelay={1000}
+                        hideLine={true}
+                    />
+                </div>
             </div>
+        );
+    }
+
+    function renderProjectCard(visible) {
+        return (
+            <Cards
+                imgPath={imgPath}
+                title={title}
+                description={description}
+                ghLink={ghLink}
+                demoLink={demoLink}
+                visible={visible}
+            />
+        );
+    }
+
+    return (
+        <div className={styles.projectCardsContainer}>
+            <Transition in={sectionVisible || focused}>
+                {({ visible }) => (
+                    <>
+                        {!alternate && !isMobile && (
+                            <>
+                                {renderDetails(visible)}
+                                {renderProjectCard(visible)}
+                            </>
+                        )}
+                        {(alternate || isMobile) && (
+                            <>
+                                {renderDetails(visible)}
+                                {renderProjectCard(visible)}
+                            </>
+                        )}
+                    </>
+                )}
+            </Transition>
         </div>
     );
 }
